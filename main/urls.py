140906456808c0824f1bab42e30a7b90221e6f9a
usmon <usmonkarimov0726@gmail.com>
2024-09-29 14:04:29 +0500

    Working on login

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name = 'main-page' ),
-    path('login',views.login, name ='login-page'),
-    path('create-account',views.create_acc,name='create-acc-page')
+    path('login',views.login_view, name ='login-page'),
+    path('create-account',views.create_acc,name='create-acc-page'),
+    path('home', views.home, name = 'home-page')
 ]
--- main/views.py
+++ main/views.py
@@ -2,20 +2,46 @@ from django.shortcuts import render,redirect
 from .forms import CreateAccountFrom
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate
 
 # Create your views here.
 def index(request):
     return render(request, 'main/index.html')
 
-def login(request):
-    
-    return render(request, 'main/login.html')
+def home(request):
+    return render(request, 'main/home.html')
+
+def login_view(request):
+    if request.method =='POST':
+        form = AuthenticationForm(request, data = request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request,user)
+                messages.success(request, "Login successfull")
+                return redirect('home-page')
+            else:
+                messages.error(request, "Invalid username or password")
+    else:
+        form = AuthenticationForm()
+    return render(request, 'main/login.html', {'form':form})
+
 
 def create_acc(request):
     if request.method =='POST':
         form = CreateAccountFrom(request.POST)
         if form.is_valid():
-            #Need to retrieve data from the form.cleaned_data and create user entry in database
+            User.objects.create_user(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+                email = form.cleaned_data['email']
+            )
+            messages.success(request, 'Your account has been created successfully! You can now log in.')
             return redirect('login-page')
     else:
         form = CreateAccountFrom()
