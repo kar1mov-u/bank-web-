42930bd7fbf38aca18c068c988c339ce813e5aeb
usmon <usmonkarimov0726@gmail.com>
2024-09-30 16:35:19 +0500

    Finished login page

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,5 @@ urlpatterns = [
     path('', views.index, name = 'main-page' ),
     path('login',views.login_view, name ='login-page'),
     path('create-account',views.create_acc,name='create-acc-page'),
-    path('home', views.home, name = 'home-page')
+    path('logout',views.log_out,name='logout-page')
 ]
--- main/views.py
+++ main/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render,redirect
 from .forms import CreateAccountFrom
+from django.db import IntegrityError
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate,logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
-
-def home(request):
-    return render(request, 'main/home.html')
+    if request.user.is_authenticated:
+        return render(request, 'main/home.html')
+    else:
+        return render(request,'main/index.html')
 
 def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('main-page')
     if request.method =='POST':
         form = AuthenticationForm(request, data = request.POST)
         if form.is_valid():
@@ -24,7 +28,7 @@ def login_view(request):
             if user is not None:
                 login(request,user)
                 messages.success(request, "Login successfull")
-                return redirect('home-page')
+                return redirect('main-page')
             else:
                 messages.error(request, "Invalid username or password")
     else:
@@ -33,17 +37,28 @@ def login_view(request):
 
 
 def create_acc(request):
+    if request.user.is_authenticated:
+        return redirect('main-page')
     if request.method =='POST':
         form = CreateAccountFrom(request.POST)
         if form.is_valid():
-            User.objects.create_user(
+            try:
+                User.objects.create_user(
                 username=form.cleaned_data['username'],
                 password=form.cleaned_data['password'],
                 email = form.cleaned_data['email']
             )
-            messages.success(request, 'Your account has been created successfully! You can now log in.')
-            return redirect('login-page')
+                messages.success(request, 'Your account has been created successfully! You can now log in.')
+                return redirect('login-page')
+            except IntegrityError:
+                messages.error(request, "This username is already taken. Please choose another.")
+                
+
     else:
         form = CreateAccountFrom()
         
     return render(request, 'main/create_acc.html',{'form':form})
+
+def log_out(request):
+    logout(request)
+    return redirect('main-page')
